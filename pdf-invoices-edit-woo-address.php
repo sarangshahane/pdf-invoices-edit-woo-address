60a4940e26811c586a55473b3af8eb88d67f7730
Sarang Shahane <sarangshahane321@gmail.com>
2024-02-02 20:01:50 +0530

    :tada: Initial Release

--- pdf-invoices-edit-woo-address.php
+++ pdf-invoices-edit-woo-address.php
@@ -3,6 +3,7 @@
  * Plugin Name: Edit WooCommerce Order Address for PDF Invoices.
  * Description: Enhance your WooCommerce experience with our plugin that seamlessly integrates with PDF Invoices. Now, easily modify the billing address of your orders before generating PDF Invoices. Our user-friendly solution conveniently places an edit button within the billing section of each order in the my-account area for quick and efficient updates. Elevate your order management process with this essential feature.
  * Author: Sarang A. Shahane
+ * Author URI: http://sarangshahane.in/
  * Version: 1.0.0
  * License: GPL v3
  * Text Domain: pdf-invoices-edit-woo-address
